f1b951e45d461d49c5b8e298e3395fdff1de3002
Andrew Hinton <hintonandrew510@gmail.com>
2025-09-14 22:06:40 +0000

    add

---
+++ webapp/src/main/java/web/powerpoint/slides/Slide.java
@@ -0,0 +1,8 @@
+package web.powerpoint.slides;
+import com.aspose.slides.*;
+
+public interface Slide {
+
+    public void populateSlide(Presentation presentation);
+
+}
--- webapp/src/main/java/web/powerpoint/slides/Slide1.java
+++ webapp/src/main/java/web/powerpoint/slides/Slide1.java
@@ -1,3 +1,10 @@
-public class Slide1 {
-    
+package web.powerpoint.slides;
+import com.aspose.slides.*;
+
+public class Slide1 implements Slide{
+
+    public void populateSlide(Presentation presentation) {
+
+    }
+
 }
