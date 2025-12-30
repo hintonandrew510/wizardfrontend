94b8fc7dae830aa7534f0db88749f855d0d88f26
hintonandrew510 <hintonandrew510@gmail.com>
2025-12-30 18:57:00 -0500

    add

---
+++ mavenprojectSCR/src/main/java/web/powerpoint/CreatePowerPointXDDFChart.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package web.powerpoint;
+
+// Source - https://stackoverflow.com/q
+// Posted by praavi, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-12-30, License - CC BY-SA 4.0
+
+import java.io.FileOutputStream;
+
+import org.apache.poi.xslf.usermodel.*;
+import org.apache.poi.util.Units;
+import org.apache.poi.xddf.usermodel.*;
+import org.apache.poi.xddf.usermodel.chart.*;
+
+import java.awt.Rectangle;
+
+public class CreatePowerPointXDDFChart {
+    
+ public static void main(String[] args) throws Exception {
+  try (XMLSlideShow slideShow = new XMLSlideShow()) {
+   
+   // create a new empty slide
+   XSLFSlide slide = slideShow.createSlide();
+   
+   // create chart
+   XSLFChart chart = slideShow.createChart();
+   chart.setTitleText("Test Chart");
+   
+   // set axis
+   XDDFCategoryAxis bottomAxis = chart.createCategoryAxis(AxisPosition.BOTTOM);
+   XDDFValueAxis leftAxis = chart.createValueAxis(AxisPosition.LEFT);
+   leftAxis.setCrosses(AxisCrosses.AUTO_ZERO);
+   leftAxis.setCrossBetween(AxisCrossBetween.BETWEEN);
+
+   // define chart data for bar chart
+   XDDFChartData data = chart.createData(ChartTypes.BAR, bottomAxis, leftAxis);
+
+   // add chart categories (x-axis data)
+   String[] categories = new String[] { "Category 1", "Category 2", "Category 3" };
+   String categoryDataRange = chart.formatRange(new org.apache.poi.ss.util.CellRangeAddress(1, categories.length, 0, 0));
+   XDDFCategoryDataSource categoryData = XDDFDataSourcesFactory.fromArray(categories, categoryDataRange, 0);
+
+   // add chart values (y-axis data)
+   Double[] values = new Double[] { 10.0, 20.0, 15.0 };
+   String valuesDataRange = chart.formatRange(new org.apache.poi.ss.util.CellRangeAddress(1, values.length, 1, 1));
+   XDDFNumericalDataSource<Double> valueData = XDDFDataSourcesFactory.fromArray(values, valuesDataRange, 1);
+   XDDFBarChartData bar = (XDDFBarChartData) data;
+   bar.setBarDirection(BarDirection.BAR);
+
+   // add series
+   XDDFChartData.Series series = data.addSeries(categoryData, valueData);
+   series.setTitle("Series 1", chart.setSheetTitle("Series 1", 1));
+   
+   // plot chart
+   chart.plot(data);
+
+   // set chart dimensions !!Units are EMU (English Metric Units)!!
+   Rectangle chartDimensions = new Rectangle(
+    100*Units.EMU_PER_POINT, 
+    50*Units.EMU_PER_POINT, 
+    400*Units.EMU_PER_POINT, 
+    400*Units.EMU_PER_POINT);
+   // add chart to slide
+   slide.addChart(chart, chartDimensions);
+
+   // Write the output to a file
+   try (FileOutputStream fileOut = new FileOutputStream("./CreatePowerPointXDDFChart.pptx")) {
+    slideShow.write(fileOut);
+   }
+  }
+ }
+
+}
---
+++ mavenprojectSCR/src/main/java/web/powerpoint/PieChart.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package web.powerpoint;
+
+// Source - https://stackoverflow.com/a
+// Posted by Axel Richter
+// Retrieved 2025-12-30, License - CC BY-SA 4.0
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xddf.usermodel.chart.*;
+import org.apache.poi.xssf.usermodel.*;
+
+public class PieChart {
+
+    public static void main(String[] args) throws IOException {
+
+        Object[][] data = new Object[][]{
+            new Object[]{"A", "B", "C"},
+            new Object[]{5d, 10d, 34d}
+        };
+
+        try (XSSFWorkbook wb = new XSSFWorkbook()) {
+            XSSFSheet sheet = wb.createSheet("piechart");
+
+            // create sheet data
+            Row row;
+            Cell cell;
+            for (int rowIndex = 0; rowIndex < 2; rowIndex++) {
+                row = sheet.createRow((short) rowIndex);
+                for (int colIndex = 0; colIndex < 3; colIndex++) {
+                    cell = row.createCell((short) colIndex);
+                    Object cellValue = data[rowIndex][colIndex];
+                    if (cellValue instanceof String) {
+                        cell.setCellValue((String) cellValue);
+                    } else if (cellValue instanceof Double) {
+                        cell.setCellValue((Double) cellValue);
+                    }
+                }
+            }
+
+            // create drawing and anchor
+            XSSFDrawing drawing = sheet.createDrawingPatriarch();
+            XSSFClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, 4, 0, 15, 15);
+
+            // create chart
+            XSSFChart chart = drawing.createChart(anchor);
+            chart.setTitleText("summary");
+            chart.setTitleOverlay(false);
+            XDDFChartLegend legend = chart.getOrAddLegend();
+            legend.setPosition(LegendPosition.TOP);
+
+            XDDFDataSource<String> cat = XDDFDataSourcesFactory.fromStringCellRange(sheet,
+                    new CellRangeAddress(0, 0, 0, 2));
+            XDDFNumericalDataSource<Double> val = XDDFDataSourcesFactory.fromNumericCellRange(sheet,
+                    new CellRangeAddress(1, 1, 0, 2));
+
+            XDDFChartData chartData = chart.createData(ChartTypes.PIE3D, null, null);
+            chartData.setVaryColors(true);
+            XDDFChartData.Series series = chartData.addSeries(cat, val);
+            chart.plot(chartData);
+
+            // add data labels
+            if (!chart.getCTChart().getPlotArea().getPie3DChartArray(0).getSerArray(0).isSetDLbls()) {
+                chart.getCTChart().getPlotArea().getPie3DChartArray(0).getSerArray(0).addNewDLbls();
+            }
+            chart.getCTChart().getPlotArea().getPie3DChartArray(0).getSerArray(0).getDLbls()
+                    .addNewShowLegendKey().setVal(false);
+            chart.getCTChart().getPlotArea().getPie3DChartArray(0).getSerArray(0).getDLbls()
+                    .addNewShowPercent().setVal(true);
+            chart.getCTChart().getPlotArea().getPie3DChartArray(0).getSerArray(0).getDLbls()
+                    .addNewShowLeaderLines().setVal(false);
+            chart.getCTChart().getPlotArea().getPie3DChartArray(0).getSerArray(0).getDLbls()
+                    .addNewShowVal().setVal(false);
+            chart.getCTChart().getPlotArea().getPie3DChartArray(0).getSerArray(0).getDLbls()
+                    .addNewShowCatName().setVal(false);
+            chart.getCTChart().getPlotArea().getPie3DChartArray(0).getSerArray(0).getDLbls()
+                    .addNewShowSerName().setVal(false);
+            chart.getCTChart().getPlotArea().getPie3DChartArray(0).getSerArray(0).getDLbls()
+                    .addNewShowBubbleSize().setVal(false);
+
+            // do not auto delete the title; is necessary for showing title in Calc
+            if (chart.getCTChart().getAutoTitleDeleted() == null) {
+                chart.getCTChart().addNewAutoTitleDeleted();
+            }
+            chart.getCTChart().getAutoTitleDeleted().setVal(false);
+
+            // data point colors
+            byte[][] rgb = new byte[][]{
+                new byte[]{127, 127, (byte) 255},
+                new byte[]{(byte) 255, 127, 127},
+                new byte[]{127, 127, 127}
+            };
+            for (int p = 0; p < 3; p++) {
+                chart.getCTChart().getPlotArea().getPie3DChartArray(0).getSerArray(0).addNewDPt().addNewIdx().setVal(p);
+                chart.getCTChart().getPlotArea().getPie3DChartArray(0).getSerArray(0).getDPtArray(p)
+                        .addNewSpPr().addNewSolidFill().addNewSrgbClr().setVal(rgb[p]);
+            }
+
+            // write the output to a file
+            try (FileOutputStream fileOut = new FileOutputStream("./ooxml-pie-chart.xlsx")) {
+                wb.write(fileOut);
+            }
+        }
+    }
+}
