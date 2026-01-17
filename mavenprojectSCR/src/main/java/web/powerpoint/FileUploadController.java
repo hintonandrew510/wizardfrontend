c8b8f603cd040a92cd4e41670d17183aa073f3b8
hintonandrew510 <hintonandrew510@gmail.com>
2026-01-17 18:15:38 -0500

    update

--- mavenprojectSCR/src/main/java/web/powerpoint/FileUploadController.java
+++ mavenprojectSCR/src/main/java/web/powerpoint/FileUploadController.java
@@ -4,8 +4,6 @@
  */
 package web.powerpoint;
 
-
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
@@ -14,52 +12,57 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.InputStream;
 import java.nio.file.Files;
-    import java.nio.file.Path;
-    import java.nio.file.Paths;
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.poi.xslf.usermodel.XSLFSlide;
 import org.apache.poi.xslf.usermodel.XSLFTextShape;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import web.data.MyUserPrincipal;
+import web.model.Contact;
+import web.powerpoint.slide.PowerPointService;
 
 @RestController
 public class FileUploadController {
 
-  private final String FILE_STORAGE_LOCATION = "/opt/wizard/download"; // Configure this path
-
-
+    private final String FILE_STORAGE_LOCATION = "/opt/wizard/download"; // Configure this path
+        @Autowired
+    private PowerPointService powerPointService;
 
     @RequestMapping(value = "/upload/slideshow/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Resource> downloadFile(@PathVariable String id) {
+    public ResponseEntity<Resource> downloadFile(@PathVariable int id, Authentication authentication) {
         try {
             // Load the file from the resources folder
             //  Resource resourced = new ClassPathResource("static/" + fileName); // Assuming files are in src/main/resources/static
-            String fileName = createPowerPoint(id);
+           // String fileName = createPowerPoint("1");
             //File downloadFile = new File(fileName);
-             Path filePath = (Path) Paths.get(FILE_STORAGE_LOCATION).resolve("output.pptx").normalize();
-             Resource resource = new UrlResource(filePath.toUri());
+            Path filePath = (Path) Paths.get(FILE_STORAGE_LOCATION).resolve("WWAY Wilmington_updated.pptx").normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            MyUserPrincipal userDetails = (MyUserPrincipal) authentication.getPrincipal();
+            Contact contact = userDetails.getContact();
+            //powerPointService.buildPowerPointDocument(id, contact);
 
-            
-          // ClassPathResource resource = new ClassPathResource("/powerpointtemplate/tv.pptx");
-         //   Resource resource = new UrlResource(filePath.toUri());
+            // ClassPathResource resource = new ClassPathResource("/powerpointtemplate/tv.pptx");
+            //   Resource resource = new UrlResource(filePath.toUri());
             if (resource.exists() && resource.isReadable()) {
                 // Determine content type
-                 String contentType = "application/vnd.openxmlformats-officedocument.presentationml.presentation"; // PPTX specific content type
+                String contentType = "application/vnd.openxmlformats-officedocument.presentationml.presentation"; // PPTX specific content type
                 String headerValue = "attachment; filename=\"" + resource.getFilename() + "\"";
 
                 //String contentType = "application/vnd.openxmlformats-officedocument.presentationml.presentation"; // Default content type
                 // You can add logic here to determine the actual content type based on file extension
                 // For example: if (fileName.endsWith(".pdf")) contentType = MediaType.APPLICATION_PDF_VALUE;
-
-               return ResponseEntity.ok()
+                return ResponseEntity.ok()
                         .contentType(MediaType.parseMediaType(contentType))
                         .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
                         .body(resource);
@@ -70,9 +73,9 @@ public class FileUploadController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-    
+
     private String createPowerPoint(String id) throws Exception {
-         // Create a ClassPathResource object for the specified file
+        // Create a ClassPathResource object for the specified file
         ClassPathResource resource = new ClassPathResource("/powerpointtemplate/radio.pptx");
 
         java.io.File file = ResourceUtils.getFile("classpath:powerpointtemplate/tv.pptx");
