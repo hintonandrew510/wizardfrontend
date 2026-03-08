720a536f5fa495c0857940c4da3778d96c0156e5
hintonandrew510 <hintonandrew510@gmail.com>
2026-03-07 19:16:39 -0500

    add

--- mavenprojectSCR/src/main/java/web/powerpoint/slide/pages/FiveStrategicMarketingPageSlide.java
+++ mavenprojectSCR/src/main/java/web/powerpoint/slide/pages/FiveStrategicMarketingPageSlide.java
@@ -1,13 +1,11 @@
 package web.powerpoint.slide.pages;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import web.google.slide.SlideReplacementData;
-
 
 import web.google.slide.SlidesData;
 import web.page.JSONManager;
@@ -19,7 +17,8 @@ import web.powerpoint.slide.AbstractSlide;
 import web.powerpoint.slide.SlidePageNameEnum;
 
 public class FiveStrategicMarketingPageSlide extends AbstractSlide {
- private static final Logger mLog = LoggerFactory.getLogger(FiveStrategicMarketingPageSlide .class.getName());
+
+    private static final Logger mLog = LoggerFactory.getLogger(FiveStrategicMarketingPageSlide.class.getName());
 
     public FiveStrategicMarketingPageSlide(SlidesData slidesData, SlidePageNameEnum slideEnum, String pageName) {
         super(slidesData, slideEnum, pageName);
@@ -27,50 +26,42 @@ public class FiveStrategicMarketingPageSlide extends AbstractSlide {
 
     @Override
     public void populateSlide(XSLFSlide slide) {
-StrategicMarketingPageOneModel model =  getmSlidesData().getPageModels().getStrategicMarketingPageOneModel();
-		StrategicMarketingPageThreeModel strategicMarketingPageThreeModel = getmSlidesData().getPageModels().getStrategicMarketingPageThreeModel();
-		
-		StrategicMarketingHelper strategicMarketingHelper =  getmSlidesData().getPageModels().getStrategicMarketingHelper();
-		List<SlideReplacementData> listData = new ArrayList<SlideReplacementData>();
-		SlideReplacementData companyNameProjectImage = new SlideReplacementData("companyNameProjectImage", model.getCompanyNameProjectImage());
-		SlideReplacementData currentCompanySlogan = new SlideReplacementData("currentCompanySlogan", model.getCurrentCompanySlogan());
-		String planToExpandBoolen = model.isPlanToExpand() ? "Yes" : "No";
-		SlideReplacementData planToExpand = new SlideReplacementData("planToExpand", planToExpandBoolen);
-		SlideReplacementData poorestMonths = new SlideReplacementData("poorestMonths", strategicMarketingHelper.getPoorestMonths());
+        StrategicMarketingPageOneModel model = getmSlidesData().getPageModels().getStrategicMarketingPageOneModel();
+        StrategicMarketingPageThreeModel strategicMarketingPageThreeModel = getmSlidesData().getPageModels().getStrategicMarketingPageThreeModel();
 
-		SlideReplacementData compliments = new SlideReplacementData("compliments",strategicMarketingPageThreeModel.getCompliments());
-		SlideReplacementData bestMonths = new SlideReplacementData("bestMonths", strategicMarketingHelper.getBestMonths());
+        StrategicMarketingHelper strategicMarketingHelper = getmSlidesData().getPageModels().getStrategicMarketingHelper();
+        List<SlideReplacementData> listData = new ArrayList<SlideReplacementData>();
+        SlideReplacementData companyNameProjectImage = new SlideReplacementData("companyNameProjectImage", model.getCompanyNameProjectImage());
+        SlideReplacementData currentCompanySlogan = new SlideReplacementData("currentCompanySlogan", model.getCurrentCompanySlogan());
+        String planToExpandBoolen = model.isPlanToExpand() ? "Yes" : "No";
+        SlideReplacementData planToExpand = new SlideReplacementData("planToExpand", planToExpandBoolen);
+        SlideReplacementData poorestMonths = new SlideReplacementData("poorestMonths", strategicMarketingHelper.getPoorestMonths());
 
-		SlideReplacementData complaint = new SlideReplacementData("complaint",strategicMarketingPageThreeModel.getComplaint());
-		SlideReplacementData bestDays = new SlideReplacementData("bestDays", strategicMarketingHelper.getBestDays());
-		
-		SlideReplacementData marketingStrategy = new SlideReplacementData("marketingStrategy",strategicMarketingPageThreeModel.getMarketingStrategy());
-		SlideReplacementData socialStrategy = new SlideReplacementData("socialStrategy", strategicMarketingPageThreeModel.getSocialStrategy());
+        SlideReplacementData compliments = new SlideReplacementData("compliments", strategicMarketingPageThreeModel.getCompliments());
+        SlideReplacementData bestMonths = new SlideReplacementData("bestMonths", strategicMarketingHelper.getBestMonths());
 
-		
-		
-		listData.add(companyNameProjectImage);
-		listData.add(currentCompanySlogan);
-		listData.add(planToExpand);
-		listData.add(poorestMonths);
-		
-		
-		listData.add(compliments);
-		listData.add(bestMonths);
-		listData.add(complaint);
-		listData.add(bestDays);
-		listData.add(marketingStrategy);
-		listData.add(socialStrategy);
-                   replaceTextOnSlide(listData, slide);
+        SlideReplacementData complaint = new SlideReplacementData("complaint", strategicMarketingPageThreeModel.getComplaint());
+        SlideReplacementData bestDays = new SlideReplacementData("bestDays", strategicMarketingHelper.getBestDays());
+
+        SlideReplacementData marketingStrategy = new SlideReplacementData("marketingStrategy", strategicMarketingPageThreeModel.getMarketingStrategy());
+        SlideReplacementData socialStrategy = new SlideReplacementData("socialStrategy", strategicMarketingPageThreeModel.getSocialStrategy());
+
+        listData.add(companyNameProjectImage);
+        listData.add(currentCompanySlogan);
+        listData.add(planToExpand);
+        listData.add(poorestMonths);
+
+        listData.add(compliments);
+        listData.add(bestMonths);
+        listData.add(complaint);
+        listData.add(bestDays);
+        listData.add(marketingStrategy);
+        listData.add(socialStrategy);
+        replaceTextOnSlide(listData, slide);
 
         //loop thru add repalce data on slide
         mLog.trace(listData.toString());
-                      
+
     }
-	
-	
-	
-
-
 
 }
